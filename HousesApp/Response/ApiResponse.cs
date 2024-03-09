a0b379ad60d5d8fd39a4ada8409d4c2891a3f581
Hussein <41930483@students.liu.edu.lb>
2024-03-09 20:39:47 +0200

    Add ApiResponse class

---
+++ HousesApp/Response/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Reflection.PortableExecutable;
+
+namespace HousesApp.Response
+{
+    public class ApiResponse
+    {
+        public HttpStatusCode  StatusCode{ get; set; }
+        public bool isSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+
+        public object Result { get; set; }
+    }
+}
