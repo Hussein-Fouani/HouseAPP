764b3acfd0cfa53661b69582ccdb5fdc13454cb1
Hussein <41930483@students.liu.edu.lb>
2024-03-09 20:39:43 +0200

    Add IRepositorys interface to define generic repository operations

---
+++ HousesApp/Repository/IRepository/IRepositorys.cs
@@ -0,0 +1,14 @@
+﻿using HousesApp.Models;
+using System.Linq.Expressions;
+
+namespace HousesApp.Repository.IRepository
+{
+    public interface IRepositories<T>where T : class
+    {
+        Task<List<T>> GetHousesAsync(Expression<Func<T, bool>> filter = null);
+        Task<T> GetHouseByIdAsync(Expression<Func<T, bool>> filter = null, bool trackChanges = true);
+        Task CreateHouseAsync(T house);
+        Task DeleteHouseAsync(T house);
+        Task Save();
+    }
+}
