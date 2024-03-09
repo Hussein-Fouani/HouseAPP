983d1cd4dc94ea6a2d1d3a308809033831ddc547
Hussein <41930483@students.liu.edu.lb>
2024-03-09 20:39:27 +0200

    Refactor HouseRepository class

--- HousesApp/Repository/HouseRepository.cs
+++ HousesApp/Repository/HouseRepository.cs
@@ -7,62 +7,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HousesApp.Repository;
 
-public class HouseRepository:IHouseRepository
+public class HouseRepository : Repository<HouseModel>, IHouseRepository
 {
     private readonly HousesDb _db;
 
-    public HouseRepository(HousesDb db)
+    public HouseRepository(HousesDb db) : base(db)
     {
         _db = db;
     }
-    public async Task<List<HouseModel>> GetHousesAsync(Expression<Func<HouseModel, bool>> filter = null)
-    {
-        IQueryable<HouseModel> houses= _db.houses;
-        if (filter != null)
-        {
-            houses=   houses.Where(filter);
-        }
-       return await houses.ToListAsync();
-    }
 
-    public async Task<HouseModel> GetHouseByIdAsync(Expression<Func<HouseModel, bool>> filter = null,bool trackChanges=true)
-    {
-        var house = _db.houses.AsQueryable();
-        if (!trackChanges)
-        {
-            house.AsTracking();
-        }
-        if(filter != null)
-            house= house.Where(filter);
-        return await _db.houses.FirstOrDefaultAsync();
-    }
 
-    
-
-    public async Task CreateHouseAsync(HouseModel house)
+    public async Task<HouseModel> UpdateHouseAsync(Guid id, HouseModel house)
     {
-        await _db.AddAsync(house);
-        await Save();
-    }
+        var houses = await _db.houses.FirstOrDefaultAsync(h => h.Id == id);
 
-    public async Task DeleteHouseAsync(HouseModel id)
-    {
-        _db.houses.Remove(id);
-        await Save();
-    }
-
-    public async Task UpdateHouseAsync(Guid id, HouseModel house)
-    {
-        var houses  =await _db.houses.FirstOrDefaultAsync(h => h.Id == id);
-       
         _db.Update(houses);
         await Save();
-        
-        
+        return houses;
+
+
     }
 
-    public async Task Save()
-    {
-      await  _db.SaveChangesAsync();
-    }
+
+
 }
